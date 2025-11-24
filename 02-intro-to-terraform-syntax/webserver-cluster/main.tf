1cd3640308dc832499c2edc6282a68656e967f31
doongjun <doongjun.kim@gmail.com>
2025-11-24 22:30:33 +0900

    웹 서버 클러스터 with LB, ASG

--- 02-intro-to-terraform-syntax/webserver-cluster/main.tf
+++ 02-intro-to-terraform-syntax/webserver-cluster/main.tf
@@ -1,31 +1,110 @@
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-04fcc2023d6e37430"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+resource "aws_launch_template" "example" {
+  name_prefix   = "terraform-asg-example-"
+  image_id      = "ami-04fcc2023d6e37430"
+  instance_type = "t3.micro"
 
-  user_data = <<-EOF
-			  #!/bin/bash
-			  cd /home/ec2-user
-			  echo "Hello, World!" > index.html
-			  python3 -m http.server ${var.server_port} >/dev/null 2>&1 &
-			  EOF
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    cd /home/ec2-user
+    echo "Hello, World!" > index.html
+    python3 -m http.server ${var.server_port} >/dev/null 2>&1 &
+  EOF
+  )
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.id
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
 
   tag {
     key                 = "Name"
     value               = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-example-lb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page Not Found"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-example-asg-tg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -44,6 +123,9 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
