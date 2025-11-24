34fa05783cd845c1ff301a01164705edda49fa0a
doongjun <doongjun.kim@gmail.com>
2025-11-24 20:57:33 +0900

    구성 가능한 웹 서버 배포

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "example" {
 			  #!/bin/bash
 			  cd /home/ec2-user
 			  echo "Hello, World!" > index.html
-			  python3 -m http.server 8080 >/dev/null 2>&1 &
+			  python3 -m http.server ${var.server_port} >/dev/null 2>&1 &
 			  EOF
 
 	user_data_replace_on_change	= true
@@ -22,9 +22,20 @@ resource "aws_security_group" "instance" {
 	name 	= "terraform-example-instance"
 
 	ingress {
-		from_port	= 8080
-		to_port		= 8080
+		from_port	= var.server_port
+		to_port		= var.server_port
 		protocol	= "tcp"
 		cidr_blocks	= ["0.0.0.0/0"]
 	}
+}
+
+variable "server_port" {
+	description	= "The port the server will use for HTTP requests"
+	type		= number
+	default		= 8080
+}
+
+output "public_ip" {
+	value		= aws_instance.example.public_ip
+	description	= "The public IP address of the web server"
 }
