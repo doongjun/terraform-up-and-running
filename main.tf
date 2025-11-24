c672dc5a67730802d06b9b23e45dbfb6550d9329
doongjun <doongjun.kim@gmail.com>
2025-11-24 17:40:26 +0900

    단일 웹 서버 배포

--- main.tf
+++ main.tf
@@ -1,8 +1,30 @@
 resource "aws_instance" "example" {
-	ami		= "ami-04fcc2023d6e37430"
-	instance_type	= "t2.micro"
+	ami			= "ami-04fcc2023d6e37430"
+	instance_type		= "t2.micro"
+	vpc_security_group_ids	= [aws_security_group.instance.id]
+	
+	user_data	= <<-EOF
+			  #!/bin/bash
+			  cd /home/ec2-user
+			  echo "Hello, World!" > index.html
+			  python3 -m http.server 8080 >/dev/null 2>&1 &
+			  EOF
 
+	user_data_replace_on_change	= true
+
+	key_name	= "terraform-example-key"
 	tags	= {
 		Name = "terraform-example"
+	}
+}
+
+resource "aws_security_group" "instance" {
+	name 	= "terraform-example-instance"
+
+	ingress {
+		from_port	= 8080
+		to_port		= 8080
+		protocol	= "tcp"
+		cidr_blocks	= ["0.0.0.0/0"]
 	}
 }
